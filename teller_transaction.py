3957e774696a437fa31ca1423cfdc9f44df43d7b
phil-bryant <phil-bryant@outlook.com>
2025-02-03 09:39:27 -0500

    Add __str__ metadata

--- teller_transaction.py
+++ teller_transaction.py
@@ -7,15 +7,12 @@ from teller_transaction_type import TellerTransactionType
 @dataclass
 class TellerTransaction(TellerObject): ## https://teller.io/docs/api/account/transactions
     account_id: str = field(default="")
-    amount: str = field(default="")
+    amount: str = field(default="", metadata={"__str__": True})
     date: str = field(default="")
-    description: str = field(default="")
+    description: str = field(default="", metadata={"__str__": True})
     details: TellerTransactionDetails = field(default=None)
-    status: str = field(default="")
+    status: str = field(default="", metadata={"__str__": True})
     id: str = field(default="")
     links: TellerTransactionLinks = field(default=None)
     running_balance: str = field(default="")
-    type: TellerTransactionType = field(default=None)
-
-    def __str__(self):
-        return f"{self.type} {self.status} {self.amount} {self.description}"
+    type: TellerTransactionType = field(default=None, metadata={"__str__": True})
