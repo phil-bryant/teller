f0390eea9baaf35e7a28a5017d888d1317f90918
phil-bryant <phil-bryant@outlook.com>
2025-02-03 08:34:29 -0500

    Eliminate __post_init__ in teller_account_links.py

--- teller_account_links.py
+++ teller_account_links.py
@@ -3,12 +3,7 @@ from teller_object import TellerObject
 
 @dataclass
 class TellerAccountLinks(TellerObject): ## https://teller.io/docs/api/accounts
-    self_link: str = field(default="")  ## self in API
+    self_link: str = field(default="", metadata={"api_name": "self"})
     details: str = field(default="")
     balances: str = field(default="")
     transactions: str = field(default="")
-
-    def __post_init__(self):
-        super().__post_init__()
-        ## Unfortunately Teller API calls this attribute "self" so we have to call it self_link due to the python reserved word, "self".
-        self.self_link = self._api_data['self']
--- teller_object.py
+++ teller_object.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 from typing import ClassVar, get_origin, get_args
 import structlog
 
@@ -12,14 +12,17 @@ class TellerObject: ## https://teller.io/docs/api
     @classmethod
     def set_api_client(cls, client):
         cls._api_client = client
-    
-    def __init__(self, api_data: dict):
-        self._api_data = api_data
+
+    def mapped_api_data(self):
+        ## Handle cases where python objects need field names differing from API field names. See next comment.
+        return {field.name: self._api_data[field.metadata.get("api_name", field.name)]
+                for field in fields(self.__class__)
+                if field.metadata.get("api_name", field.name) in self._api_data}
         
     def __post_init__(self):
-        ## Use python introspection to simply mirror the Teller API Objects as python objects.
-        log.debug("TellerObject.__post_init__", class_name=self.__class__.__name__, api_data=self._api_data)
-        for key, value in self._api_data.items():
+        ## Use python introspection to otherwise simply mirror the Teller API Objects as python objects.
+        log.debug("TellerObject.__post_init__", class_name=self.__class__.__name__, api_data=self._api_data, mapped_api_data=self.mapped_api_data())
+        for key, value in self.mapped_api_data().items():
             if hasattr(self, key) and value is not None:
                 if get_origin(self.__annotations__[key]) is list:
                     setattr(self, key, [get_args(self.__annotations__[key])[0](element) for element in value])
