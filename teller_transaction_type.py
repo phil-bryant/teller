9dead11ef6efd94ec19b5a712b1290a145fbfa1a
phil-bryant <phil-bryant@outlook.com>
2025-02-03 00:29:05 -0500

    Python objects working again now

--- teller_transaction_type.py
+++ teller_transaction_type.py
@@ -5,5 +5,12 @@ from teller_object import TellerObject
 class TellerTransactionType(TellerObject):
     code: str = field(default="")
 
+    def __init__(self, api_data):
+        if isinstance(api_data, str):
+            self.code = api_data
+            self._api_data = {"code": api_data}
+        else:
+            super().__init__(api_data)
+
     def __str__(self):
-        return self.code 
+        return self.code
