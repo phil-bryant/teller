edd58a5198906987af3edd6ec9da404c8b70aac5
phil-bryant <phil-bryant@outlook.com>
2025-02-03 13:00:57 -0500

    more TellerList research

--- teller_list.py
+++ teller_list.py
@@ -4,12 +4,11 @@ import structlog
 log = structlog.get_logger()
 
 class TellerList(list):
-    __orig_type__=None
-    def __class_getitem__(cls,item):
-        cls.__orig_type__=item
-        log.info("TellerList.__class_getitem__", item=item)
-        return cls
-    def __new__(cls,*a,**b):
-        i=super(TellerList,cls).__new__(cls,*a,**b)
-        log.info("Observing TellerList usage at runtime", typed_item=cls.__orig_type__)
-        return i
+    def __class_getitem__(cls, item):
+        class SubTellerList(cls):
+            __orig_type__ = item
+            def __new__(subcls, *a, **b):
+                i = super(SubTellerList, subcls).__new__(subcls, *a, **b)
+                log.info("Observing TellerList usage", typed_item=subcls.__orig_type__)
+                return i
+        return SubTellerList
