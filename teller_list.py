32d76d97551a490d3c1640644fb976dcb4cd7c0f
phil-bryant <phil-bryant@outlook.com>
2025-02-03 13:10:18 -0500

    Ugly but TellerList seems to be working

--- teller_list.py
+++ teller_list.py
@@ -11,4 +11,13 @@ class TellerList(list):
                 i = super(SubTellerList, subcls).__new__(subcls, *a, **b)
                 log.info("Observing TellerList usage", typed_item=subcls.__orig_type__)
                 return i
+            def __init__(self, data=None):
+                conv = []
+                if data is None:
+                    pass
+                elif isinstance(data, dict):
+                    conv.append(self.__orig_type__(data))
+                else:
+                    conv = [self.__orig_type__(x) for x in data]
+                super(SubTellerList, self).__init__(conv)
         return SubTellerList
--- teller_object.py
+++ teller_object.py
@@ -27,10 +27,7 @@ class TellerObject: ## https://teller.io/docs/api
         log.debug("TellerObject.__post_init__", class_name=self.__class__.__name__, api_data=self._api_data, mapped_api_data=self._mapped_api_data())
         for key, value in self._mapped_api_data().items():
             if hasattr(self, key) and value is not None:
-                if get_origin(self.__annotations__[key]) is list:
-                    setattr(self, key, [get_args(self.__annotations__[key])[0](element) for element in value])
-                else:
-                    setattr(self, key, self.__annotations__[key](value)) 
+                setattr(self, key, self.__annotations__[key](value)) 
 
     def __str__(self):
         return f"{self.__class__.__name__}({', '.join(f'{getattr(self, name)}' for name in self._str_field_names())}):_api_data={self._api_data}"
