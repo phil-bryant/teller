3d1955a14d3c0afb83d04df876344c80af7f601c
phil-bryant <phil-bryant@outlook.com>
2025-02-03 13:39:38 -0500

    Consider cleaning TellerList further in the future

--- teller_list.py
+++ teller_list.py
@@ -1,4 +1,7 @@
-
+## This is rather basic functionality that python itself should have implemented
+## this allows one to call setattr(variableName, variableValue) in cases where variableName was declared like:
+##   names: TellerList[TellerIdentityName] = field(default_factory=list)
+## such that names will then be instantiated as a list of TellerIdentityName instances upon calling setattr(...)
 class TellerList(list):
     def __class_getitem__(cls, item):
         class SubTellerList(cls):
