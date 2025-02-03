e66c15b8cc93034439e030288a0db2bb22765a17
phil-bryant <phil-bryant@outlook.com>
2025-02-03 00:03:59 -0500

    might kinda work

--- teller_transaction_details.sql
+++ teller_transaction_details.sql
@@ -3,14 +3,9 @@ CREATE TABLE teller.transaction_details (
     category teller.transaction_category,
     counterparty_id BIGINT REFERENCES teller.transaction_details_counterparty(transaction_details_counterparty_id),
     transaction_details_id BIGSERIAL PRIMARY KEY,
-    transaction_id TEXT NOT NULL UNIQUE REFERENCES teller.transaction(transaction_id),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 COMMENT ON TABLE teller.transaction_details IS 'Table for teller_transaction_details.py';
-COMMENT ON COLUMN teller.transaction_details.category IS 'The category that the transaction belongs to';
 COMMENT ON COLUMN teller.transaction_details.processing_status IS 'Indicates the transaction enrichment processing status';
-COMMENT ON COLUMN teller.transaction_details.merchant_name IS 'The name of the merchant';
-COMMENT ON COLUMN teller.transaction_details.merchant_website IS 'The website of the merchant if available';
-COMMENT ON COLUMN teller.transaction_details.check_number IS 'The check number if applicable';
-COMMENT ON COLUMN teller.transaction_details.type IS 'The type of transaction'; 
+COMMENT ON COLUMN teller.transaction_details.category IS 'The category that the transaction belongs to';
