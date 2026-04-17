44bc60f9fe08282fae98a17ce8bf5d70fc905cec
phil-bryant <phil-bryant@outlook.com>
2026-04-17 10:10:26 -0400

    teller_transaction_info_view

---
+++ teller_transaction_info_view.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE VIEW teller.transaction_info_view 
+AS	SELECT 		ta.institution_id, tt.amount, tt.date, tt.description, ttt.code, ttd.category,
+       			ttdc.name AS counterparty_name, ttdc.type AS counterparty_type
+	FROM 		teller.transaction tt
+				LEFT JOIN teller.account ta USING (account_id)
+				LEFT JOIN teller.transaction_type ttt USING (transaction_type_id)
+				LEFT JOIN teller.transaction_details ttd USING (transaction_details_id)
+				LEFT JOIN teller.transaction_details_counterparty ttdc USING (transaction_details_counterparty_id)
+				ORDER BY tt.date, tt.description
+;
