1559b6f60afb2401c0d03b62252d16fc5140cadc
phil-bryant <phil-bryant@outlook.com>
2026-04-18 08:02:38 -0400

    Add nys_snw category

--- teller_transaction_nys_snw_category.sql
+++ teller_transaction_nys_snw_category.sql
@@ -1,9 +1,13 @@
+CREATE TYPE teller.transaction_categorization_method AS ENUM ('user', 'ai');
+COMMENT ON TYPE teller.transaction_categorization_method IS 'How was a transaction classified: manually by the user or automatically via ai';
+
 CREATE TABLE teller.transaction_nys_snw_category (
     transaction_id TEXT NOT NULL REFERENCES teller.transaction(transaction_id),
     nys_snw_category_id BIGINT NOT NULL REFERENCES teller.nys_snw_category(nys_snw_category_id),
+    type teller.transaction_categorization_method NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
-COMMENT ON TABLE teller.transaction_nys_snw_category IS 'Table for classifying transactions';
+COMMENT ON TABLE teller.transaction_nys_snw_category IS 'Table for storing transaction (re)classifications';
 COMMENT ON COLUMN teller.transaction_nys_snw_category.transaction_id IS 'Reference to the id of the transaction';
-COMMENT ON COLUMN teller.transaction_nys_snw_category.transaction_id IS 'Reference to the id of the category';
+COMMENT ON COLUMN teller.transaction_nys_snw_category.nys_snw_category_id IS 'Reference to the id of the category';
